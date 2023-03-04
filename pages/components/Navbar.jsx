f0259646b525d01dc46c33a041bfd6c91c2a1228
MaxChuloshnikov <Maks447@ukr.net>
2023-03-04 12:36:49 +0200

    ShopingPage Done

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW!</div>
-          <div className={styles.text}>+ 777 77 77 77</div>
+          <div className={styles.text}>+ 777 87 78 78</div>
         </div>
         </div>
     

9067bc2977a54897cdba4b6e4f42fbbe757fcbb6
MaxChuloshnikov <Maks447@ukr.net>
2023-03-04 15:17:35 +0200

    done

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../../styles/Order.module.css';
+
+const Order = () => {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default Order;
